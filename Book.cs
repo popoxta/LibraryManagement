bd7bc3bd7094798b4635d26ec2ab95fb9768a61f
lari <popoxta@gmail.com>
2025-03-12 16:01:24 +1300

    Create backing field for Title to handle title transformation into titlecase/trmi

--- Book.cs
+++ Book.cs
@@ -1,6 +1,20 @@
-﻿namespace LibraryManagement;
+﻿using System.Globalization;
+
+namespace LibraryManagement;
 
 public class Book
 {
-    public required string Title { init; get; }
+    private readonly TextInfo _textInfo = new CultureInfo("en-US", false).TextInfo;
+    private readonly string _title = "";
+
+    public required string Title
+    {
+        init => _title = TitleCaseBookTitle(value);
+        get => _title;
+    }
+
+    private string TitleCaseBookTitle(string title)
+    {
+        return _textInfo.ToTitleCase(title.Trim().ToLower());
+    }
 }
