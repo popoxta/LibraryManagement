0898c26e6c9253bb71b7fb073e3a71adccdf3d7c
lari <popoxta@gmail.com>
2025-03-13 19:32:50 +1300

    Change Menu -> UserInterface and implement Menu

---
+++ UserInterface.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Spectre.Console;
+
+namespace LibraryManagement;
+
+[method: SetsRequiredMembers]
+internal class UserInterface(BooksController booksController)
+{
+    private enum MenuOption
+    {
+        ViewBooks,
+        AddBook,
+        DeleteBook,
+    }
+
+    internal required BooksController BooksController { get; init; } = booksController;
+
+    private static MenuOption GetMenuOption() =>
+        AnsiConsole
+            .Prompt(new SelectionPrompt<MenuOption>()
+                .Title("Select an option")
+                .AddChoices(Enum.GetValues<MenuOption>()));
+
+
+    public void MainMenu()
+    {
+        while (true)
+        {
+            Console.Clear();
+            var option = GetMenuOption();
+
+            switch (option)
+            {
+                case MenuOption.ViewBooks:
+                    BooksController.ViewBooks();
+                    break;
+                case MenuOption.AddBook:
+                    BooksController.AddBook();
+                    break;
+                case MenuOption.DeleteBook:
+                    BooksController.DeleteBook();
+                    break;
+                default:
+                    throw new ArgumentException("Invalid menu option!");
+            }
+
+            BooksController.PressKeyToContinue();
+        }
+    }
+}
