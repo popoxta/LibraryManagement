e048654550f98c9b90ac1eaf523e2d951dea69de
lari <popoxta@gmail.com>
2025-03-16 11:15:28 +1300

    Set up all books in a mock DB - stater books

--- Program.cs
+++ Program.cs
@@ -2,17 +2,9 @@
 
 internal static class Program
 {
-    private static readonly string[] StarterBooks =
-    [
-        "The Great Gatsby", "To Kill a Mockingbird", "1984", "Pride and Prejudice", "The Catcher in the Rye",
-        "The Hobbit", "Moby-Dick", "War and Peace", "The Odyssey", "The Lord of the Rings", "Jane Eyre", "Animal Farm",
-        "Brave New World", "The Chronicles of Narnia", "The Diary of a Young Girl", "The Alchemist",
-        "Wuthering Heights", "Fahrenheit 451", "Catch-22", "The Hitchhiker's Guide to the Galaxy"
-    ];
-
     private static readonly Library Library = new(books:
     [
-        ..StarterBooks.Select(title => new Book
+        ..MockDatabase.StarterBooks.Select(title => new Book
         {
             Title = title,
         })
