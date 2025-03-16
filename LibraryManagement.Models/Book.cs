002d3e57576a6975189b4bc014022bd51d8c6a86
lari <popoxta@gmail.com>
2025-03-17 06:36:33 +1300

    Implement models implementing abstract LibraryItem class

---
+++ LibraryManagement.Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using Spectre.Console;
+
+namespace LibraryManagement.Models;
+
+public class Book : LibraryItem<Book>
+{
+    public required string Category { get; init; }
+    public required string Author { get; init; }
+
+    public override void DisplayDetails()
+    {
+        var panel = new Panel(new Markup($"[bold]Book:[/] [cyan]{Title}[/] by [cyan]{Author}[/]") +
+                              $"\n[bold]Pages:[/] {Pages}" +
+                              $"\n[bold]Category:[/] [green]{Category}[/]" +
+                              $"\n[bold]Location:[/] [blue]{Location}[/]")
+        {
+            Border = BoxBorder.Rounded
+        };
+
+        AnsiConsole.Write(panel);
+    }
+}
--- LibraryManagement.Models/Magazine.cs
+++ LibraryManagement.Models/Magazine.cs
@@ -2,15 +2,17 @@
 
 namespace LibraryManagement.Models;
 
-public class NewsPaper : LibraryItem<NewsPaper>
+public class Magazine : LibraryItem<Magazine>
 {
     public required string Publisher { get; init; }
     public required DateTime PublishDate { get; init; }
+    public required int IssueNumber { get; init; }
 
     public override void DisplayDetails()
     {
-        var panel = new Panel(new Markup($"[bold]Newspaper:[/] [cyan]{Title}[/] published by [cyan]{Publisher}[/]") +
+        var panel = new Panel(new Markup($"[bold]Magazine:[/] [cyan]{Title}[/] published by [cyan]{Publisher}[/]") +
                               $"\n[bold]Publish Date:[/] {PublishDate:yyyy-MM-dd}" +
+                              $"\n[bold]Issue Number:[/] {IssueNumber}" +
                               $"\n[bold]Location:[/] [blue]{Location}[/]")
         {
             Border = BoxBorder.Rounded
