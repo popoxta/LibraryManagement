75f263f8c75d3d4c8eb98254949d9a9f6b8806cf
lari <popoxta@gmail.com>
2025-03-17 06:31:36 +1300

    Implement common methods for library items

--- LibraryManagement.Models/LibraryItem.cs
+++ LibraryManagement.Models/LibraryItem.cs
@@ -1,10 +1,28 @@
-﻿namespace LibraryManagement.Models;
+﻿using System.Globalization;
 
-internal abstract class LibraryItem(int id, string name, string location)
+namespace LibraryManagement.Models;
+
+public abstract class LibraryItem<T> where T : LibraryItem<T>
 {
-    public required int Id { get; init; } = id;
-    public required string Name { get; init; } = name;
-    public required string Location { get; init; } = location;
+    private readonly TextInfo _textInfo = new CultureInfo("en-US", false).TextInfo;
+
+    public Guid Id { get; } = Guid.NewGuid();
+    public required string Location { get; init; }
+    public required int Pages { get; init; }
+
+    private readonly string _title = "";
+
+    public required string Title
+    {
+        init => _title = TitleCaseTitle(value);
+        get => _title;
+    }
 
     public abstract void DisplayDetails();
+
+    private string TitleCaseTitle(string title) => _textInfo.ToTitleCase(title.Trim().ToLower());
+
+    public override bool Equals(object? other) => other is T item && Title == item.Title;
+
+    public override int GetHashCode() => Title.GetHashCode();
 }
