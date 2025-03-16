c75fc2b5e2049c19807fb7ed61473885afc1b731
lari <popoxta@gmail.com>
2025-03-16 13:42:33 +1300

    Create abstract LibraryItem class

---
+++ LibraryManagement.Models/LibraryItem.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryManagement.Models;
+
+internal abstract class LibraryItem(int id, string name, string location)
+{
+    public required int Id { get; init; } = id;
+    public required string Name { get; init; } = name;
+    public required string Location { get; init; } = location;
+
+    public abstract void DisplayDetails();
+}
