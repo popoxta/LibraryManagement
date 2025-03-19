2d2107ef3d70f0c8eaa83d8fd8bcc126a5da6470
lari <popoxta@gmail.com>
2025-03-19 18:45:40 +1300

    Remove unused ConfirmDeletion method.

--- LibraryManagement.Controllers/BaseController.cs
+++ LibraryManagement.Controllers/BaseController.cs
@@ -6,7 +6,4 @@ public abstract class BaseController
 {
     protected static void DisplayMessage(string message, ConsoleColor color = ConsoleColor.Yellow) =>
         AnsiConsole.MarkupLine($"{message}", color);
-
-    protected static bool ConfirmDeletion(string itemName) =>
-        AnsiConsole.Confirm($"Are you sure you want to delete {itemName}?");
 }
